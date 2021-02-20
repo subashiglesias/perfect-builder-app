7e0c77983c78f7c451795a38d2be045cbf857052
Subash P <subash.p@mrcooper.com>
2021-02-20 16:25:15 +0530

    First Deployment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <div className="App">
         <header>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>We now have Auth!</h1>
+          <h1>Welcome to Perfect Builders</h1>
+          <h3>This site is currently under construction, visit us after some time</h3>
         </header>
         <AmplifySignOut />
       </div>
