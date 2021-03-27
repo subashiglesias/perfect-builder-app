d6af3dd38807da622343d769a46c5da1fb781099
Subash P <subash.p@mrcooper.com>
2021-03-27 23:36:16 +0530

    Added react table

--- src/components/ProjectsPage/ProjectsPage.jsx
+++ src/components/ProjectsPage/ProjectsPage.jsx
@@ -1,16 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import './ProjectsPage.scss';
-import {makeStyles} from "@material-ui/core/styles";
-import {TabContext, TabPanel} from "@material-ui/lab";
-import TabList from "@material-ui/lab/TabList";
-import Tab from "@material-ui/core/Tab";
 import 'reactjs-popup/dist/index.css';
-import addBox from '../../images/add_box.svg'
-import ProjectsForm from "../Forms/ProjectsForm";
 import {API, Auth} from "aws-amplify";
 import {listProjects} from "../../graphql/queries";
 import {createProject as createProjectMutation} from '../../graphql/mutations';
-import {Avatar} from "@material-ui/core";
+import styled from "styled-components";
+import ReactTable from "../ReactTable";
 
 
 var moment = require('moment'); // require
@@ -66,43 +61,146 @@ const ProjectsPage = () => {
         }
     }
 
-    const useStyles = makeStyles(theme => ({
-        root: {
-            flexGrow: 1,
-            display: 'flex',
-        },
-        tabs: {
-            borderRight: `1px solid ${theme.palette.divider}`,
-        }
-    }));
-    const classes = useStyles();
+    const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-collapse: collapse;
+    margin: 25px 0;
+    font-size: 0.9em;
+    font-family: sans-serif;
+    min-width: 400px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+
+    thead tr {
+        background-color: #009879;
+        color: #ffffff;
+        text-align: left;
+    }
+
+    th {
+        background-color: #282c34;
+        color: #ffffff;
+        text-align: center;
+        padding-top: 10px;
+        padding-bottom: 10px;
+    },
+    td {
+      padding: 12px 15px;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+    
+    th:last-of-type {
+        padding-left:1rem;
+        padding-right:2rem;
+    }
+    
+    tbody tr {
+    border-bottom: 1px solid #dddddd;
+    }
+    
+    tbody tr:nth-of-type(even) {
+    background-color: #f3f3f3;
+    }
+    
+    tbody tr:last-of-type {
+    border-bottom: 2px solid #009879;
+    }
+    
+    tbody tr.active-row {
+    font-weight: bold;
+    color: #009879;
+    }
+  }
+`
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: 'Projects',
+                columns: [
+                    {
+                        Header: 'Name',
+                        accessor: 'projectName',
+                    },
+                    {
+                        Header: 'Address',
+                        accessor: 'projectAddress',
+                    },
+                    {
+                        Header: 'Blocks Count',
+                        accessor: 'projectBlocksCount',
+                    },
+                    {
+                        Header: 'Floors Count',
+                        accessor: 'projectFloorsCount',
+                    },
+                    {
+                        Header: 'Created By',
+                        accessor: 'createdBy',
+                    },
+                    {
+                        Header: 'Created Date',
+                        accessor: 'createdDate',
+                    },
+                    {
+                        Header: 'Edit or Delete',
+                        accessor: 'editOrDelete',
+                    },
+                ],
+            },
+        ],
+        []
+    )
+
+    const formatData = (projects) => {
+        const data = []
+        projects.map(project => data.push({
+            projectName: project.name,
+            projectAddress: project.address,
+            projectBlocksCount: project.noOfBlocks,
+            projectFloorsCount: 'yet to implement',
+            createdBy: project.createdBy,
+            createdDate: project.createdDate
+        }))
+        return data;
+    }
+
+
+
+
+
     return (
         <div className="project-page">
-            <div className={classes.root}>
-                <TabContext value={value}>
-                    <TabList orientation="vertical"
-                             variant="scrollable"
-                             value={value}
-                             onChange={handleChange}
-                             aria-label="List of projects"
-                             className={classes.tabs}>
-                        <Tab icon={<Avatar alt="Add new" src={addBox} />} value={0}/>
-                        {projects.map((project) => (<Tab label={project.name} value={project.name}/>))}
-                    </TabList>
-                    <div className="project-page__panel">
+            <Styles>
+                <ReactTable columns={columns} data={formatData(projects)} />
+            </Styles>
+                {/*<TabContext value={value}>*/}
+                {/*    <TabList orientation="vertical"*/}
+                {/*             variant="scrollable"*/}
+                {/*             value={value}*/}
+                {/*             onChange={handleChange}*/}
+                {/*             aria-label="List of projects"*/}
+                {/*             className={classes.tabs}>*/}
+                {/*        <Tab icon={<Avatar alt="Add new" src={addBox} />} value={0}/>*/}
+                {/*        {projects.map((project) => (<Tab label={project.name} value={project.name}/>))}*/}
+                {/*    </TabList>*/}
+                {/*    <div className="project-page__panel">*/}
 
-                    </div>
-                    <TabPanel value={0}>
-                        <div className="project-page__content">
-                            <h3> Please select the name of the projects at the side to edit or fill a new one below</h3>
-                            <ProjectsForm handleSubmit={handleSubmit} dialog={dialog} fieldValues={{}} classes={classes}/>
-                        </div>
-                    </TabPanel>
-                    {projects.map((project) => (
-                        <TabPanel value={project.name}> <ProjectsForm handleSubmit={handleSubmit} dialog={dialog}
-                                                                      fieldValues={project} classes={classes}/> </TabPanel>))}
-                </TabContext>
-            </div>
+                {/*    </div>*/}
+                {/*    <TabPanel value={0}>*/}
+                {/*        <div className="project-page__content">*/}
+                {/*            <h3> Please select the name of the projects at the side to edit or fill a new one below</h3>*/}
+                {/*            <ProjectsForm handleSubmit={handleSubmit} dialog={dialog} fieldValues={{}} classes={classes}/>*/}
+                {/*        </div>*/}
+                {/*    </TabPanel>*/}
+                {/*    {projects.map((project) => (*/}
+                {/*        <TabPanel value={project.name}> <ProjectsForm handleSubmit={handleSubmit} dialog={dialog}*/}
+                {/*                                                      fieldValues={project} classes={classes}/> </TabPanel>))}*/}
+                {/*</TabContext>*/}
+
         </div>)
 }
 
